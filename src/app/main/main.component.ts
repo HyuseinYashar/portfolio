0703bbbd90aee8f509235397a2a8d37be71a0537
Hyusein Yashar <hyashar96@gmail.com>
2025-01-19 22:22:33 +0100

    skills peel

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [HeroComponent,AboutMeComponent],
+  imports: [HeroComponent,AboutMeComponent,SkillsComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
--- src/app/main/skills/skills.component.ts
+++ src/app/main/skills/skills.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
 
+
+peel() {
+  
+}
+
+  skills = [
+    {svg: '/assets/img/skills/html.svg' , label : 'HTML'},
+    {svg: '/assets/img/skills/css.svg' , label : 'CSS'},
+    {svg: '/assets/img/skills/skills8.svg' , label : 'JavaScript'},
+    {svg: '/assets/img/skills/skills7.svg' , label : 'Typescript'},
+    {svg: '/assets/img/skills/python.svg' , label : 'Python'},
+    {svg: '/assets/img/skills/skills4.svg' , label : 'API'},
+    {svg: '/assets/img/skills/skills6.svg' , label : 'Firebase'},
+    {svg: '/assets/img/skills/restapi.svg' , label : 'RestApi'},
+    {svg: '/assets/img/skills/skills5.svg' , label : 'GIT'},
+    {svg: '/assets/img/skills/skills2.svg' , label : 'Material Design'}
+  ];
+ 
 }
